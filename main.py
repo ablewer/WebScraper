8209658023bed86b606b16f8fa461e0980aa35f1
Shawn Ringler <ringlersm@gmail.com>
2019-10-31 08:12:35 -0600

    Testing web-scrapping prototype. Need to use API with .JSON files.

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+# I understand the want to use of JSON files and the API now. Just using the the web-scrapping module will result in a
+# a lot of pain for ourselves.
+
+import requests
+import bs4
+
 
 # A test of the github features
 
-print('This is a test of Main.py in the GitHUB')
+url = 'https:www.google.com/search?q=tree+farms+colorado'
+
+res = requests.get(url)
+
+p_elems = bs4.BeautifulSoup(res.text, 'html.parser')
+
